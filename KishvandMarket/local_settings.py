fc76716702e2a5add6067d2e88d679a7ba87e1a2
Hojjatrt <hojjatrt@gmail.com>
2019-08-07 13:11:26 +0430

    add local settings file

---
+++ KishvandMarket/local_settings.py
@@ -0,0 +1,24 @@
+# Grabs the site root setup in settings.py
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+INSTALLED_APPS = [
+    'dal',
+    'dal_select2',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'django_filters',
+    'market.apps.MarketConfig',
+    'userapp.apps.UserappConfig',
+    'sorl.thumbnail',
+]
+
+# sqlite is the quick an easy development db
+
+BASE_URL = u'http://kishvand.pythonanywhere.com'

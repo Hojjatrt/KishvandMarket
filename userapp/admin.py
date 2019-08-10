553b1941c1eb35107e5dd10f2fbf6917df60b1f1
Hojjatrt <hojjatrt@gmail.com>
2019-08-10 11:29:14 +0430

    update user admin

--- userapp/admin.py
+++ userapp/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from userapp.models import *
 # Register your models here.
+
+######################
+######################
+
+
+class MyUserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone', 'usertype', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('username', 'phone', 'first_name', 'last_name', 'is_staff')
 
 ######################
 ######################
@@ -15,4 +31,5 @@ class AddressAdmin(admin.ModelAdmin):
 ######################
 ######################
 
-# admin.site.register(Address, AddressAdmin)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(User, MyUserAdmin)
